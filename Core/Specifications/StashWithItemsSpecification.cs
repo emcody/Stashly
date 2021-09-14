d3fa11a6580c4da784fbfd159c177cc1bf8ff6b2
emcody <gorczix@gmail.com>
2021-09-14 18:03:59 +0200

    Add more options for ordering stashes

--- Core/Specifications/StashWithItemsSpecification.cs
+++ Core/Specifications/StashWithItemsSpecification.cs
@@ -8,6 +8,28 @@ namespace Core.Specifications
         {
             AddInclude(x => x.Items);
             AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "ownerIdAsc":
+                        AddOrderBy(s => s.OwnerId);
+                        break;
+                    case "ownerIdDesc":
+                        AddOrderByDescending(s => s.OwnerId);
+                        break;
+                    case "locationAsc":
+                        AddOrderBy(s => s.Location);
+                        break;
+                    case "locationDesc":
+                        AddOrderByDescending(s => s.Location);
+                        break;
+                    default:
+                        AddOrderBy(x => x.Name);
+                        break;
+                }
+            }
         }
 
         public StashWithItemsSpecification(int id) : base(x => x.Id == id)
