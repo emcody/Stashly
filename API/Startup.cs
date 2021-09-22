6ec0e824a37a7d18f893848758f036608acd55d6
emcody <gorczix@gmail.com>
2021-09-22 18:09:55 +0200

    Add CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -47,6 +47,13 @@ namespace API
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,16 +61,14 @@ namespace API
         {
             app.UseMiddleware<ExceptionMiddleware>();
 
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            // }
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
