58a2f26e5bd1c1b53351c35b493b47293c8a623a
emcody <gorczix@gmail.com>
2021-09-28 10:04:31 +0200

    Add user register method

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,7 +1,8 @@
 namespace API.Dtos
 {
-    public class LoginDto
+    public class RegisterDto
     {
+        public string DisplayName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
